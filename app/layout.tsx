f684615676817658c0b051cac8d19258c2ddf514
NizarAbiZaher <Nizabizaher@gmail.com>
2024-04-26 08:28:55 -0500

    initial commit / setup

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nizzy-Starter",
+  description: "The best saas starter kit on the web 🚀",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
