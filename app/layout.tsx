c5989ce1592de28f8686e2a74a9c30159b38c451
NizarAbiZaher <Nizabizaher@gmail.com>
2024-05-12 10:45:16 -0500

    updated desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nizzy-Starter",
-  description: "The best SaaS starter kit on the web 🌎 🚀",
+  description: "The best SaaS starter kit on the web 🌎 🚀 HAHA",
 };
 
 export default function RootLayout({
