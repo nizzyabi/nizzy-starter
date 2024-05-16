ef07d10378e2f3ebfd325b0795e3649457aa6ecd
NizarAbiZaher <Nizabizaher@gmail.com>
2024-05-16 14:34:01 -0400

    todo

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 export const Social = () => {
-    // add onClick event to the button
+    // TODO:add onClick event to the button
     return (
         <div className="flex gap-2 mt-3">
             <Button className="rounded-[5px] w-full border border-primary/20 bg-secondary text-primary hover:bg-primary/10 text-md">
