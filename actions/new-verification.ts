11e8df87a0b2f3ba64f442aeba827ab3721228ca
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-05-17 08:12:00 -0400

    actioions

---
+++ actions/new-verification.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+
+import { getVerificationTokenByToken } from "@/data/verification-token";
+
+export const newVerification = async (token:string) => {
+
+    
+    // if no token, display message
+    const exisitingToken = await getVerificationTokenByToken(token);
+
+    if(!exisitingToken) {
+        return { error: "Token does not exisit 😣" };
+    }
+    // if token has expired, display message
+    const hasExpired = new Date(exisitingToken.expires) < new Date();
+
+    if (hasExpired) {
+        return { error: "Token has expired 😣" };
+    }
+    // if user does not exist, display message
+    const existingUser = await getUserByEmail(exisitingToken.email);
+
+    if (!existingUser) {
+        return { error: "User does not exisit 😬" };
+    }
+    // update email value when they verify
+    await db.user.update({
+        where: { id: existingUser.id },
+        data: {
+            emailVerified: new Date(),
+            email: exisitingToken.email
+        }
+    })
+    // delete token
+    await db.verificationToken.delete({
+        where: { id: exisitingToken.id }
+    });
+
+    return { success: "Email verified 🎉. Go to login to continue"}
+
+    
+    
+}
