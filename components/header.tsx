cb4ec8766c99395939af530abd20b6d61aec987c
NizarAbiZaher <Nizabizaher@gmail.com>
2024-05-23 07:40:18 -0400

    height

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,13 @@
 import { Sparkles } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
 
 export const Header = () => {
     return (
         <div className="space-y-20">
-        <div className="max-w-6xl mx-auto  px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="text-center">
-            <div className="items-center md:pt-12 pt-0">
+            <div className="items-center xs:pt-0 md:pt-8 lg:pt-10 xl:pt-12 pt-4">
               <h2 className="text-4xl font-extrabold  sm:text-5xl">Clone. Build. Ship.</h2>
               <p className="mt-4 text-lg text-primary/50">
               Build your SaaS faster with our fully customizable template.
@@ -21,60 +22,19 @@ export const Header = () => {
             </div>
             </div>
           </div>
-          <div className="md:block rounded-lg shadow-lg overflow-hidden lg:mx-12" style={{ aspectRatio: '16/9' }}>
+          <div className="md:block rounded-lg shadow-lg overflow-hidden" style={{ aspectRatio: '16/9' }}>
             <iframe
                 title="Product Video"
                 src="https://www.youtube.com/embed/VIDEO_ID" // Replace VIDEO_ID with your video ID or video URL
-                width="640"
-                height="360"
+                width="680"
+                height="400"
                 className="object-cover w-full h-full"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
             ></iframe>
           </div>
         </div>
-        <div className="mt-20 flex justify-center space-x-8">
-          <img
-            alt="Next.js"
-            height="24"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "80/24",
-              objectFit: "cover",
-            }}
-            width="80"
-          />
-          <img
-            alt="Stripe Logo"
-            height="24"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "80/24",
-              objectFit: "cover",
-            }}
-            width="80"
-          />
-          <img
-            alt="Resend"
-            height="24"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "80/24",
-              objectFit: "cover",
-            }}
-            width="80"
-          />
-          <img
-            alt="DB"
-            height="24"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "80/24",
-              objectFit: "cover",
-            }}
-            width="80"
-          />
-        </div>
+       
       </div>
     )
 }
