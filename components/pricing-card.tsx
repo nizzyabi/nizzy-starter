229d79bd1c11c8b525b0f41a310b70d892b4010f
NizarAbiZaher <Nizabizaher@gmail.com>
2024-06-05 16:36:12 -0400

    comeptitor name

--- components/pricing-card.tsx
+++ components/pricing-card.tsx
@@ -8,7 +8,7 @@ import axios from "axios";
 // Update Tiers Here
 export const tiers = [
   {
-    name: "Your Competitors Name",
+    name: "Your Competitor Name",
     price: '18.99',
     features: ["Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7", "Feature 8", "Feature 9", "Feature 10"],
     cta: "Get Started",
