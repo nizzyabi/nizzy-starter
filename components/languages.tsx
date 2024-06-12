2498ab14f59fa0d319c7495e0947198d5e56bf6f
NizarAbiZaher <Nizabizaher@gmail.com>
2024-06-12 09:06:25 -0400

    centered languages

--- components/languages.tsx
+++ components/languages.tsx
@@ -14,15 +14,16 @@ const languages = [
     className: 'px-2 opacity-50 hover:opacity-100 transition-opacity duration-300 ease-in-out cursor-pointer'
   },
   {
-    srcLight: '/language/next-dark.png',
-    srcDark: '/language/next-light.png',
-    className: 'px-2 opacity-50 hover:opacity-100 transition-opacity duration-300 ease-in-out cursor-pointer'
-  },
-  {
     srcLight: '/language/tailwind.png',
     srcDark: '/language/tailwind.png',
     className: 'px-2 opacity-50 hover:opacity-100 transition-opacity duration-300 ease-in-out cursor-pointer'
   },
+  {
+    srcLight: '/language/next-dark.png',
+    srcDark: '/language/next-light.png',
+    className: 'px-2 opacity-50 hover:opacity-100 transition-opacity duration-300 ease-in-out cursor-pointer'
+  },
+  
 ];
 
 export function Language() {
@@ -33,7 +34,7 @@ export function Language() {
       <div>
         <div className="container mx-auto px-4 md:px-20">
           <div className="relative">
-            <div className="grid grid-cols-2 place-items-center gap-2 md:gap-4 lg:grid-cols-4 xl:gap-x-6 2xl:grid-cols-8">
+            <div className="grid grid-cols-2 place-items-center gap-2 md:gap-4 lg:grid-cols-4 ">
               {languages.map((language, idx) => (
                 <Image
                   key={idx}
