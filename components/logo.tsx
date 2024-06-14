3c4102ef81c30a686096af6d64f0e36b343f8423
dpaulos6 <itzframepvp@outlook.com>
2024-06-14 11:09:36 +0100

    Logo Icon Update

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,9 @@
-import { RandomIcon } from '@/components/icons'
+import { Squircle } from 'lucide-react'
 
 export const Logo = () => {
   return (
     <div className="flex items-center gap-2 group">
-      <RandomIcon className="w-12 h-12 text-primary group-hover:-rotate-12 transition-all duration-300" />
+      <Squircle className="w-12 h-12 text-primary group-hover:-rotate-12 transition-all duration-300" />
       <span className="text-xl group-hover:translate-x-0.5 transition-all duration-300">
         YourCompany
       </span>
