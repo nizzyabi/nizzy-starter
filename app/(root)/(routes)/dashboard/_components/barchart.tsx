c1e8eee021dc6b780dd1768ccc28d88bba0fd087
NizarAbiZaher <Nizabizaher@gmail.com>
2024-07-08 08:50:43 -0400

    fixed bar chart issue

--- app/(root)/(routes)/dashboard/_components/barchart.tsx
+++ app/(root)/(routes)/dashboard/_components/barchart.tsx
@@ -15,7 +15,6 @@ import { light_theme } from '@/lib/theme-constant'
 import { CandlestickChart } from 'lucide-react'
 import { ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
 
-
 const chartConfig = {
   desktop: {
     label: "Desktop",
@@ -40,43 +39,44 @@ export default function BarChart({ data }: BarChartProps) {
         <p>Sales Data</p>
         <CandlestickChart className="h-4 w-4" />
       </section>
-      <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-        <BarGraph
-          data={data}
-          margin={{ top: 20, left: -10, right: 10, bottom: 0 }}
-        >
-          <CartesianGrid
-            strokeDasharray="2 2"
-            stroke={`${theme === light_theme ? '#000' : '#fff'}`}
-            className="opacity-25"
-          />
-          <XAxis
-            dataKey={'month'}
-            tickLine={false}
-            axisLine={true}
-            stroke={`${theme === light_theme ? '#000' : '#fff'}`}
-            fontSize={13}
-            padding={{ left: 0, right: 0 }}
-          />
-          <ChartTooltip  content={<ChartTooltipContent />} />
-          <YAxis
-            tickLine={false}
-            axisLine={true}
-            stroke={`${theme === light_theme ? '#000' : '#fff'}`}
-            fontSize={13}
-            padding={{ top: 0, bottom: 0 }}
-            allowDecimals={false}
-            tickFormatter={(value) => `$${value}`}
-          />
-
-          <Legend />
-          <Bar
-            dataKey={'total'}
-            radius={[5, 5, 0, 0]}
-            stroke="hsl(var(--primary))"
-            fill="hsl(var(--primary))"
-          />
-        </BarGraph>
+      <ChartContainer config={chartConfig} className="">
+        <ResponsiveContainer width="100%" height={300}>
+          <BarGraph
+            data={data}
+            margin={{ top: 20, left: -10, right: 10, bottom: 0 }}
+          >
+            <CartesianGrid
+              strokeDasharray="2 2"
+              stroke={`${theme === light_theme ? '#000' : '#fff'}`}
+              className="opacity-25"
+            />
+            <XAxis
+              dataKey={'month'}
+              tickLine={false}
+              axisLine={true}
+              stroke={`${theme === light_theme ? '#000' : '#fff'}`}
+              fontSize={13}
+              padding={{ left: 0, right: 0 }}
+            />
+            <ChartTooltip  content={<ChartTooltipContent />} />
+            <YAxis
+              tickLine={false}
+              axisLine={true}
+              stroke={`${theme === light_theme ? '#000' : '#fff'}`}
+              fontSize={13}
+              padding={{ top: 0, bottom: 0 }}
+              allowDecimals={false}
+              tickFormatter={(value) => `$${value}`}
+            />
+            <Legend />
+            <Bar
+              dataKey={'total'}
+              radius={[5, 5, 0, 0]}
+              stroke="hsl(var(--primary))"
+              fill="hsl(var(--primary))"
+            />
+          </BarGraph>
+        </ResponsiveContainer>
       </ChartContainer>
     </div>
   )
