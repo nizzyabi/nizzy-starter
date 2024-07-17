8dde960a1cdb852d879a3e887ee7466f1bb7da8b
NizarAbiZaher <Nizabizaher@gmail.com>
2024-07-17 15:07:38 -0400

    management

--- app/api/emails/route.ts
+++ app/api/emails/route.ts
@@ -1,29 +1,47 @@
 import { Resend } from "resend"
 import RepoEmail from "@/components/email/email";
 import { render } from "@react-email/render"
+import { NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST (request: Request, res: Response) {
+export async function POST(request: Request) {
+    try {
+        const { email } = await request.json();
 
-    const { email } = await request.json();
+        const { data, error } = await resend.emails.send({
+            from: "Nizzy <noreply@nizzystarter.com>",
+            to: [email],
+            subject: "Nizzy Starter Kit",
+            html: render(RepoEmail()),
+        });
 
-    const { data, error } = await resend.emails.send({
-        from: "Nizzy <noreply@nizzystarter.com>",
-        to: [email],
-        subject: "Nizzy Starter Kit",
-        html: render(RepoEmail()),
-    })
+        if (error) {
+            return new NextResponse(JSON.stringify(error), {
+                status: 500,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+        }
 
-    if (error) {
-        return Response.json(error)
+        await resend.contacts.create({
+            email: email,
+            audienceId: process.env.RESEND_AUDIENCE as string
+        });
+
+        return new NextResponse(JSON.stringify({ message: "Email sent successfully" }), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } catch (error:any) {
+        return new NextResponse(JSON.stringify({ error: error.message }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
     }
-
-    resend.contacts.create({
-        email: email,
-        audienceId: process.env.RESEND_AUDIENCE as string
-    })
-
-    return Response.json({ message: "Email sent successfully" })
-
 }
