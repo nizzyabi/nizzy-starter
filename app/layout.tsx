f865ef83be7026aef4ddd6a25193ac321cf56087
Samuel Braun <office.samigo.a@gmail.com>
2024-11-05 15:25:15 +0100

    fix: :bug: Fixed hydration missmatch error/warning. See https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
   const session = await auth()
   return (
     <SessionProvider session={session}>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
